038eb33dcecc9b3e3146e84eb94612bca0229d6f
frannav <frannavmart@gmail.com>
2024-04-02 21:26:22 +0100

    WIP: add shadcn ui, react and tailwind

---
+++ src/components/AvatarComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+export const AvatarComponent = ({ src, alt, fallbackText }) => {
+  return (
+    <Avatar>
+      <AvatarImage src={src} alt={alt}/>
+      <AvatarFallback>{fallbackText}</AvatarFallback>
+    </Avatar>
+  );
+};
